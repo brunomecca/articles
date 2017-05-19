bee2c3140dcab193605b6ffcbc686dcfed7d3857
Bruno Mecca <brunogmecca@gmail.com>
2017-05-19 11:34:51 -0400

    inicio do projeto
    definindo estrutura

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+	$link = mysqli_connect("localhost","root","","articles");
+
+	mysqli_query($link,"SET NAMES 'utf8'");
+	mysqli_query($link,'SET character_set_connection=utf8');
+	mysqli_query($link,'SET character_set_client=utf8');
+	mysqli_query($link,'SET character_set_results=utf8');
+?>
